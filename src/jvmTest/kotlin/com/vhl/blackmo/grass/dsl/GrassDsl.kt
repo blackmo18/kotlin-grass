d6592b76a1d19ae8ae9cd9e6d21e06e45494ee75
Victor Harlan D. Lacson <vh.lacson18@gmail.com>
2020-06-22 07:29:58 +0800

    Added new setting trim white space

--- src/jvmTest/kotlin/com/vhl/blackmo/grass/dsl/GrassDsl.kt
+++ src/jvmTest/kotlin/com/vhl/blackmo/grass/dsl/GrassDsl.kt
@@ -2,24 +2,27 @@ package com.vhl.blackmo.grass.dsl
 
 import com.vhl.blackmo.grass.pot.Plant
 import io.kotlintest.matchers.types.shouldBeTypeOf
+import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 @ExperimentalStdlibApi
-class GradeDsl: StringSpec({
+class GrassDsl: StringSpec({
     data class Sample(val sample: String)
     "instantiate grass parser globally" {
         val grass = grass<Sample>()
         grass.shouldBeTypeOf<Plant<Sample>>()
     }
 
-    "able to input setting in dsl" {
+    "able to input settings in dsl" {
         val grass = grass<Sample>{
             timeFormat = "HH-mm-ss"
             dateFormat = "MM-dd-yyyy"
+            trimWhiteSpace = false
             dateTimeSeparator = "-"
         }
-        grass.ctx.timeFormat = "HH-mm-ss"
-        grass.ctx.dateFormat = "MM-dd-yyyy"
-        grass.ctx.dateTimeSeparator = "-"
+        grass.ctx.timeFormat shouldBe  "HH-mm-ss"
+        grass.ctx.dateFormat shouldBe  "MM-dd-yyyy"
+        grass.ctx.dateTimeSeparator shouldBe  "-"
+        grass.ctx.trimWhiteSpace shouldBe  false
     }
 })
